7aaf8c96582e7a797c057c256c0d4309a4d78eee
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2023-01-01 18:25:20 +0200

    lesson 8

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
-  <div id="app">
-    <TheButton>Click me</TheButton>
+  <div :id="$style.app">
+    <h1>{{ title }}</h1>
+    <!-- <TheButton @click="inctrement" outlined>Click me</TheButton> -->
+    <!-- <DirectivesTest /> -->
+    <StarRating :rating="4.5" />
   </div>
 </template>
 
 <script>
-import TheButton from './components/TheButton.vue'
+// import TheButton from './components/TheButton.vue'
+// import DirectivesTest from './components/DirectivesTest.vue'
+import StarRating from './components/StarRating.vue'
 
 export default {
   name: 'App',
   components: {
-    TheButton
+    // TheButton,
+    // DirectivesTest,
+    StarRating
+  },
+  data() {
+    return {
+      amountOfClicks: 0
+    }
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClicks}`
+    }
+  },
+  methods: {
+    inctrement() {
+      this.amountOfClicks += 1
+    }
   }
 }
 </script>
 
-<style>
+<style module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button v-bind:type="type">
+    <button v-on="$listeners" v-bind:type="type" :class="{btn: true, 'btn--outlined': outlined}">
         <slot> </slot>
     </button>
 </template>
@@ -11,11 +11,30 @@
             type: {
                 type: String,
                 default: 'button'
+            },
+            outlined: {
+                type: Boolean,
+                default: false
             }
         }        
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+.btn {
+        display: inline-block;
+        font-size: 18px;
+        background: #ff6620;
+        color: #fff;
+        cursor: pointer;
+        min-width: 220px;
+        border: 1px solid transparent;
+        padding: 8px 15px;
 
+        &--outlined {
+            background: none;
+            border: 1px solid #ff6620;
+            color: #ff6620;
+        }
+    }
 </style>
